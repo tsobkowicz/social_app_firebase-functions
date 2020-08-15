b3ebbc9ed6274c9325bf4b552a9cbfa3b2d15576
TomS <tomaszsobkowicz21@gmail.com>
2020-08-15 15:12:40 +0200

    Add first firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,48 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+admin.initializeApp();
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
+});
+
+exports.getScreams = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        // data function returns the data that's inside the document
+        screams.push(doc.data());
+      });
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+});
+
+exports.createScream = functions.https.onRequest((req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(400).json({ error: 'method not allowed' });
+  }
+  const { body, userHandle } = req.body;
+  const newScream = {
+    body,
+    userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+  admin
+    .firestore()
+    .collection('screams')
+    .add(newScream)
+    // eslint-disable-next-line promise/always-return
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'something went wrong' });
+      console.log(err);
+    });
 });
