468507224c3b003935daf0e678e67ab68a22d7bb
TomS <tomaszsobkowicz21@gmail.com>
2020-08-16 19:44:51 +0200

    Update ESLintconfig, convert to async/await and spread operator

--- functions/index.js
+++ functions/index.js
@@ -5,46 +5,85 @@ const express = require('express');
 admin.initializeApp();
 const app = express();
 
-app.get('/screams', (req, res) => {
-  admin
-    .firestore()
-    .collection('screams')
-    .orderBy('createdAt', 'desc')
-    .get()
-    .then((data) => {
-      let screams = [];
-      data.forEach((doc) => {
-        screams.push({
-          screamId: doc.id,
-          body: doc.data().body,
-          userHandle: doc.data().userHandle,
-          createdAt: doc.data().createdAt,
-        });
+app.get('/screams', async (req, res) => {
+  try {
+    const data = await admin
+      .firestore()
+      .collection('screams')
+      .orderBy('createdAt', 'desc')
+      .get();
+
+    let screams = [];
+    data.forEach((doc) => {
+      screams.push({
+        screamId: doc.id,
+        ...doc.data(),
       });
       return res.json(screams);
-    })
-    .catch((err) => console.error(err));
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-app.post('/scream', (req, res) => {
+app.post('/scream', async (req, res) => {
   const { body, userHandle } = req.body;
   const newScream = {
     body,
     userHandle,
     createdAt: new Date().toISOString(),
   };
-  admin
-    .firestore()
-    .collection('screams')
-    .add(newScream)
-    // eslint-disable-next-line promise/always-return
-    .then((doc) => {
-      res.json({ message: `document ${doc.id} created successfully` });
-    })
-    .catch((err) => {
-      res.status(500).json({ error: 'something went wrong' });
-      console.log(err);
-    });
+  try {
+    const doc = await admin.firestore().collection('screams').add(newScream);
+
+    res.json({ message: `document ${doc.id} created successfully` });
+  } catch (err) {
+    res.status(500).json({ error: 'something went wrong' });
+    console.log(err);
+  }
 });
+
+// app.get('/screams', (req, res) => {
+//   admin
+//     .firestore()
+//     .collection('screams')
+//     .orderBy('createdAt', 'desc')
+//     .get()
+//     .then((data) => {
+//       let screams = [];
+//       data.forEach((doc) => {
+//         screams.push({
+//           screamId: doc.id,
+//           ...doc.data(),
+//           // body: doc.data().body,
+//           // userHandle: doc.data().userHandle,
+//           // createdAt: doc.data().createdAt,
+//         });
+//       });
+//       return res.json(screams);
+//     })
+//     .catch((err) => console.error(err));
+// });
+
+// app.post('/scream', (req, res) => {
+//   const { body, userHandle } = req.body;
+//   const newScream = {
+//     body,
+//     userHandle,
+//     createdAt: new Date().toISOString(),
+//   };
+//   admin
+//     .firestore()
+//     .collection('screams')
+//     .add(newScream)
+//     // eslint-disable-next-line promise/always-return
+//     .then((doc) => {
+//       res.json({ message: `document ${doc.id} created successfully` });
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ error: 'something went wrong' });
+//       console.log(err);
+//     });
+// });
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
